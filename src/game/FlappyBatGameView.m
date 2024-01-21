d7a210201e0cf049ac41e3cbe21919bc2b06679c
Marek Wdowiak <marek.wdowiak@pw.edu.pl>
2024-01-21 15:41:09 +0100

    Create template file for game view

---
+++ src/game/FlappyBatGameView.m
@@ -0,0 +1,18 @@
+classdef FlappyBatGameView < handle
+
+    properties (Access = public)
+        MainWindow % (1,1)
+    end
+
+    properties (Access = private)
+    end
+
+    methods (Access = public)
+        % Constructor
+        function this = FlappyBatGameView()
+        end
+
+        function render(this, gameModel)
+        end
+    end
+end
